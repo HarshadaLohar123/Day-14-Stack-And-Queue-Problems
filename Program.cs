87be50988ab9644153eb29466dec13b7b4452a54
harshadalohar123 <harshalohar123@gmail.com>
2022-03-27 11:20:11 +0530

    ADDED:Create A Stack..

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Uc_1_Stack
+{
+    public class CreateStack
+    {
+        private class Node
+        {
+            public int data;
+            public Node link;
+        }
+        Node top;
+        public CreateStack()
+        {
+            this.top = null;
+        }
+
+        public void Push(int x)
+        {
+            Node temp = new Node();
+
+            temp.data = x;
+            temp.link = top;
+            top = temp;
+        }
+
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("The Elements of Stack Is:");
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write("{0} ", temp.data);
+                    temp = temp.link;
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            CreateStack obj = new CreateStack();
+
+            obj.Push(56);
+            obj.Push(30);
+            obj.Push(70);
+
+            obj.Display();
+
+        }
+    }
+}
